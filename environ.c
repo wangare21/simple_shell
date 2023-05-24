c1fa2e5b57273d8d57970a7d03d9aa02f7e43323
Lista-KM <mngeikerubo@gmail.com>
2023-05-23 21:52:16 -0700

    Shell

---
+++ environ.c
@@ -0,0 +1,103 @@
+#include "main.h"
+
+
+/**
+ * _myenv - This one prints the current environment
+ * @info: This is Structure containing potential arguments.
+ * It is used to maintain constant function prototype.
+ * Return: This will Always return a 0
+ */
+int _myenv(ino_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+
+/**
+ * _getenv - getenv gets the value of an environ variable
+ * @info: This is the Structure containing potential arguments.
+ * It is used to maintain constant function prototype.
+ * @name: name is the env variable name
+ *
+ * Return: The function will return the value
+ */
+char *_getenv(ino_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+
+/**
+ * _mysetenv - mysetenv will Initialise a new environment variable,
+ *             or it will modify an existing one
+ * @info: This is the Structure containing potential arguments.
+ * It is used to maintain constant function prototype.
+ *  Return: The function will Always return 0.
+ */
+int _mysetenv(ino_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+
+/**
+ * _myunsetenv - It will Remove an environment variable
+ * @info: This is the Structure containing potential arguments.
+ * It is used to maintain constant function prototype.
+ * Return: This will Always 0
+ */
+int _myunsetenv(ino_t *info)
+{
+	int i;
+
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+
+	return (0);
+}
+
+
+/**
+ * populate_env_list - This one populates env linked list
+ * @info: This is Structure containing potential arguments.
+ *  It is used to maintain constant function prototype.
+ * Return: This will Always return 0.
+ */
+int populate_env_list(ino_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _eputs - This one prints an input string
+ * @str: str is the string to be printed
+ *
+ * Return: It returns Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - This writes the character c to stderr
+ * @c: c the character to print
+ *
+ * Return: On success it returns 1.
+ * On error, -1 is returned, and err no is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - This writes the character c to given fd
+ * @c: This is the character to print
+ * @fd: This the file descriptor to write to
+ *
+ * Return: On success it returns 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - This one prints an input string
+ * @str: str the string to be printed
+ * @fd: fd is the file descriptor to write to
+ *
+ * Return: it returns the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
